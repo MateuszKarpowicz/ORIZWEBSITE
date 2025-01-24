6f38f561e4cfb1138eb5daf506dcb3dafcc3bdf1
MateuszKarpowicz <mateuszkarpowicz@gmail.com>
2025-01-24 13:35:15 +0100

    building structure

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -4,6 +4,13 @@ import Logo from '../../ui/Logo/Logo';
 function Navbar() {
   return (
     <nav className="navbar">
+      <div className="navbar__container__upper">
+        <div className="navbar__section navbar__section--left">
+          <a href="/cooperation" className="navbar__link">WSPÓŁPRACA</a>
+          <a href="/contact" className="navbar__link">KONTAKT</a>
+        </div>
+
+      
       <div className="navbar__section navbar__section--left">
         <a href="/cooperation" className="navbar__link">WSPÓŁPRACA</a>
         <a href="/contact" className="navbar__link">KONTAKT</a>
@@ -16,6 +23,19 @@ function Navbar() {
       <div className="navbar__section navbar__section--right">
         <a href="/about" className="navbar__link">O NAS</a>
         <a href="/generator" className="navbar__button">GENERATOR OFERT</a>
+      </div>
+      
+      </div>
+      <div className="navbar__container__lower">
+        <div className="navbar__filter">
+         <div className="navbar__filter__button">
+          <button className="navbar__filter__button__item">WYCIECZKI ZAGRANICZNE</button>
+          <button className="navbar__filter__button__item">WYCIECZKI KRAJOWE</button>
+          <button className="navbar__filter__button__item">OBOZY SPORTOWE</button>
+          <button className="navbar__filter__button__item">OBOZY JĘZYKOWE</button>
+          <button className="navbar__filter__button__item">ZAPROPONUJ WYCIECZKĘ</button>
+          </div>
+        </div>
       </div>
     </nav>
   );
