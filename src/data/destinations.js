c61194d6e951c84ca973437db7d21a7f0a715dca
MateuszKarpowicz <mateuszkarpowicz@gmail.com>
2025-01-24 13:48:33 +0100

    push

---
+++ src/data/destinations.js
@@ -0,0 +1,116 @@
+export const destinations = [
+  {
+    id: 'londyn',
+    title: 'LONDYN',
+    slug: 'londyn',
+    priceFrom: 1099,
+    thumbnail: '/images/destinations/londyn-thumb.jpg',
+    mainImage: '/images/destinations/londyn-main.jpg',
+    description: `Londyn to miasto, które nigdy nie przestaje zachwycać. 
+    Historyczne zabytki jak Big Ben czy Tower Bridge harmonijnie współgrają 
+    z nowoczesnymi budynkami jak The Shard. Spacerując po Covent Garden, 
+    możesz poczuć prawdziwy klimat brytyjskiej stolicy...`
+  },
+  {
+    id: 'malta',
+    title: 'MALTA',
+    slug: 'malta',
+    priceFrom: 1099,
+    thumbnail: '/images/destinations/malta-thumb.jpg',
+    mainImage: '/images/destinations/malta-main.jpg',
+    description: `Malta to wyspiarski kraj, gdzie historia spotyka się 
+    z krystalicznie czystym morzem. Średniowieczne miasta, złote plaże 
+    i błękitne laguny tworzą niezapomnianą mozaikę wrażeń...`
+  },
+  {
+    id: 'barcelona',
+    title: 'BARCELONA',
+    slug: 'barcelona',
+    priceFrom: 1199,
+    thumbnail: '/images/destinations/barcelona-thumb.jpg',
+    mainImage: '/images/destinations/barcelona-main.jpg',
+    description: `Barcelona to miasto Gaudiego, modernizmu i katalońskiej dumy. 
+    Sagrada Familia, Park Güell i La Rambla to tylko początek atrakcji. 
+    Lokalne tapas, plaże i niepowtarzalna atmosfera...`
+  },
+  {
+    id: 'usa',
+    title: 'USA',
+    slug: 'usa',
+    priceFrom: 4999,
+    thumbnail: '/images/destinations/usa-thumb.jpg',
+    mainImage: '/images/destinations/usa-main.jpg',
+    description: `Stany Zjednoczone to kraj nieograniczonych możliwości. 
+    Od Nowego Jorku po Los Angeles, od Wielkiego Kanionu po Hawaje...`
+  },
+  {
+    id: 'azja',
+    title: 'AZJA',
+    slug: 'azja',
+    priceFrom: 3999,
+    thumbnail: '/images/destinations/azja-thumb.jpg',
+    mainImage: '/images/destinations/azja-main.jpg',
+    description: `Azja to kontynent kontrastów. Nowoczesne metropolie 
+    sąsiadują tu z pradawnymi świątyniami. Odkryj smaki, zapachy i kolory Orientu...`
+  },
+  {
+    id: 'ateny',
+    title: 'ATENY',
+    slug: 'ateny',
+    priceFrom: 1299,
+    thumbnail: '/images/destinations/ateny-thumb.jpg',
+    mainImage: '/images/destinations/ateny-main.jpg',
+    description: `Ateny to kolebka europejskiej cywilizacji. Akropol, 
+    Partenon i starożytna Agora przypominają o wielkich początkach...`
+  },
+  {
+    id: 'neapol',
+    title: 'NEAPOL',
+    slug: 'neapol',
+    priceFrom: 1199,
+    thumbnail: '/images/destinations/neapol-thumb.jpg',
+    mainImage: '/images/destinations/neapol-main.jpg',
+    description: `Neapol to miasto pełne pasji. Najlepsza pizza na świecie, 
+    Wezuwiusz i ruiny Pompejów tworzą niezapomniane włoskie doświadczenie...`
+  },
+  {
+    id: 'teneryfa',
+    title: 'TENERYFA',
+    slug: 'teneryfa',
+    priceFrom: 2099,
+    thumbnail: '/images/destinations/teneryfa-thumb.jpg',
+    mainImage: '/images/destinations/teneryfa-main.jpg',
+    description: `Teneryfa to wyspa wiecznej wiosny. Wulkan Teide, 
+    czarne plaże i malownicze miasteczka tworzą raj na Atlantyku...`
+  },
+  {
+    id: 'edynburg',
+    title: 'EDYNBURG',
+    slug: 'edynburg',
+    priceFrom: 1399,
+    thumbnail: '/images/destinations/edynburg-thumb.jpg',
+    mainImage: '/images/destinations/edynburg-main.jpg',
+    description: `Edynburg to miasto legend i festiwali. Średniowieczne 
+    Stare Miasto, zamek na wzgórzu i szkocka gościnność...`
+  },
+  {
+    id: 'dublin',
+    title: 'DUBLIN',
+    slug: 'dublin',
+    priceFrom: 1299,
+    thumbnail: '/images/destinations/dublin-thumb.jpg',
+    mainImage: '/images/destinations/dublin-main.jpg',
+    description: `Dublin to miasto literatury i muzyki. Temple Bar, 
+    Guinness Storehouse i irlandzka gościnność czekają...`
+  },
+  {
+    id: 'lizbona',
+    title: 'LIZBONA',
+    slug: 'lizbona',
+    priceFrom: 1499,
+    thumbnail: '/images/destinations/lizbona-thumb.jpg',
+    mainImage: '/images/destinations/lizbona-main.jpg',
+    description: `Lizbona to miasto siedmiu wzgórz. Tramwaje, fado, 
+    pastéis de nata i oceaniczne widoki tworzą portugalską magię...`
+  }
+]; 
