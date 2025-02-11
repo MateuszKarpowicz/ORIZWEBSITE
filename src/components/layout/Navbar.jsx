ede8aab2dd7a5b7b19c899966def5d7d91d68df6
Duckjaniels <maksym73@gmail.com>
2025-02-11 14:51:44 +0100

    Fixed navbar version

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -38,33 +38,115 @@ function Navbar() {
 
   return (
     <nav className="navbar">
-      <div className="navbar__container__upper">
-        <div className="navbar__section navbar__section--left">
-          <Link to="/references" className="navbar__link">
-            REFERENCJE
-          </Link>
-          <Link to="/contact" className="navbar__link">
-            KONTAKT
-          </Link>
+      {/* Wersja desktopowa */}
+      <div className="navbar__desktop">
+        <div className="navbar__container__upper">
+          <div className="navbar__section navbar__section--left">
+            <Link to="/references" className="navbar__link">
+              REFERENCJE
+            </Link>
+            <Link to="/contact" className="navbar__link">
+              KONTAKT
+            </Link>
+          </div>
+
+          <div className="navbar__section navbar__section--center">
+            <Logo />
+          </div>
+
+          <div className="navbar__section navbar__section--right">
+            <Link to="/about" className="navbar__link">
+              O NAS
+            </Link>
+            <Link to="/generator" className="navbar__button">
+              GENERATOR OFERT
+            </Link>
+          </div>
         </div>
 
-        <div className="navbar__section navbar__section--center">
-          <Logo />
-        </div>
-
-        <div className="navbar__section navbar__section--right">
-          <Link to="/about" className="navbar__link">
-            O NAS
-          </Link>
-          <Link to="/generator" className="navbar__button">
-            GENERATOR OFERT
-          </Link>
+        <div className="navbar__container__lower">
+          <div className="navbar__filter">
+            <div className="navbar__filter__button">
+              {categories.map((category) => (
+                <Link
+                  key={category.id}
+                  to={category.path}
+                  className={`navbar__filter__button__item ${
+                    location.pathname === category.path
+                      ? "navbar__filter__button__item--active"
+                      : ""
+                  }`}
+                >
+                  {category.label}
+                </Link>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className="navbar__container__lower">
-        <div className="navbar__filter">
-          <div className="navbar__filter__button">
+      {/* Wersja mobilna */}
+      <div className="navbar__mobile-container">
+        <div className="navbar__mobile">
+          <Logo />
+          <button
+            className={`hamburger ${isMenuOpen ? "hamburger--active" : ""}`}
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+        </div>
+
+        <div
+          className={`navbar__mobile-menu ${
+            isMenuOpen ? "navbar__mobile-menu--active" : ""
+          }`}
+        >
+          <div className="navbar__mobile-links">
+            <Link
+              to="/references"
+              className="navbar__link"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              REFERENCJE
+            </Link>
+            <Link
+              to="/contact"
+              className="navbar__link"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              KONTAKT
+            </Link>
+            <Link
+              to="/about"
+              className="navbar__link"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              O NAS
+            </Link>
+            <Link
+              to="/generator"
+              className="navbar__button"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              GENERATOR OFERT
+            </Link>
+          </div>
+
+          <button
+            className="navbar__categories-toggle"
+            onClick={() => setIsCategoriesOpen(!isCategoriesOpen)}
+          >
+            KATEGORIE
+          </button>
+
+          <div
+            className={`navbar__categories-menu ${
+              isCategoriesOpen ? "navbar__categories-menu--active" : ""
+            }`}
+          >
             {categories.map((category) => (
               <Link
                 key={category.id}
@@ -74,6 +156,10 @@ function Navbar() {
                     ? "navbar__filter__button__item--active"
                     : ""
                 }`}
+                onClick={() => {
+                  setIsMenuOpen(false);
+                  setIsCategoriesOpen(false);
+                }}
               >
                 {category.label}
               </Link>
